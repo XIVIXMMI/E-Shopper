355ea11cb2e4077a97aebf087e72fc78d7dc378d
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-07-12 02:07:12 +0700

    product detail page

---
+++ E-Shopper/Components/JustArrived.cs
@@ -0,0 +1,20 @@
+﻿using E_Shopper.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shopper.Components
+{
+    public class JustArrived : ViewComponent 
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JustArrived(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Products.Where(p=>p.IsArrived==true).ToList());
+        }
+
+    }
+}
---
+++ E-Shopper/Components/Trandy.cs
@@ -0,0 +1,20 @@
+﻿using E_Shopper.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shopper.Components
+{
+    public class Trandy : ViewComponent 
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Trandy(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Products.Where(p=>p.IsTrandy==true).ToList());
+        }
+
+    }
+}
